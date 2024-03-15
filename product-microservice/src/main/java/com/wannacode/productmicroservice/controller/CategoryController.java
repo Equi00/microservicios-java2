90c2a011e5d9e0237834ecf32ba728d13e65379e
Equi00 <ezeloyola3@gmail.com>
2024-03-15 19:21:31 -0300

    incluimos el refresco de configuracion

---
+++ product-microservice/src/main/java/com/wannacode/productmicroservice/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.wannacode.productmicroservice.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/categories")
+@Getter
+@Setter
+public class CategoryController {
+
+    @Value("${app.testProp}")
+    private String testProp;
+
+    // este endpoint es para testear si cuando enviamos un commit desde nuestra configuracion centralizada
+    // se esta reflejando tambien en nuestro product microservice sin la necesidad de detener e iniciar el microservicio
+    public String getTestProp(){
+        return this.testProp;
+    }
+}
