98630b5d85149a92735f7dbdd3f1be82ee6070d8
Equi00 <ezeloyola3@gmail.com>
2024-03-15 19:29:07 -0300

    pruebas

--- product-microservice/src/main/java/com/wannacode/productmicroservice/controller/CategoryController.java
+++ product-microservice/src/main/java/com/wannacode/productmicroservice/controller/CategoryController.java
@@ -3,6 +3,8 @@ package com.wannacode.productmicroservice.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/categories")
 @Getter
 @Setter
+@RefreshScope
 public class CategoryController {
 
     @Value("${app.testProp}")
@@ -17,6 +20,7 @@ public class CategoryController {
 
     // este endpoint es para testear si cuando enviamos un commit desde nuestra configuracion centralizada
     // se esta reflejando tambien en nuestro product microservice sin la necesidad de detener e iniciar el microservicio
+    @GetMapping("test-prop")
     public String getTestProp(){
         return this.testProp;
     }
